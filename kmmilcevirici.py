982d6d9827ff605c7887b20d3ca20805f320d886
Fatih Arda ZENGİN <Fatih61@users.noreply.github.com>
2020-02-16 22:08:37 +0200

    Add files via upload

--- kmmilcevirici.py
+++ kmmilcevirici.py
@@ -1,4 +1,11 @@
-#ver0.1
-print("Km->Mil çeviriciye hos geldiniz.")
-sayi=int(input("Km'yi giriniz :"))
-print("Mil :"+ str(sayi/1.609344))
+#ver1.0
+print("Km->Mil çeviriciye hos geldiniz.\n Lutfen Seciniz \n 1) Km-> Mil \n 2)Mil->Km")
+ss=int(input("Seçiminizi yaziniz :"))
+if(ss==1):
+    sayi=int(input("Km'yi giriniz :"))
+    print("Mil :"+ str(sayi/1.609344))
+elif(ss==2):
+    sayi=int(input("Mil'i giriniz :"))
+    print("Km :"+ str(sayi*1.609344))
+else:
+    print("Hatalı giriş yaptınız")
